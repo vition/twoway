2cb0f90a0f0846c0b03c03b8b7bbb680b7b5df38
vition <369709991@qq.com>
2017-04-01 18:35:25 +0800

    login

--- Application/Home/Controller/AdminController.class.php
+++ Application/Home/Controller/AdminController.class.php
@@ -3,6 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class AdminController extends Controller {
     public function index(){
-        echo "管理员这里看";
+       $this->display("login");
     }
 }

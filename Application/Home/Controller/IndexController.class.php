b007a58cbaeec4e655260653a23be0047c4fba17
vition <369709991@qq.com>
2017-04-18 16:06:13 +0800

    修改ckeditor

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+    	$this->assign("about","./Public/html/".md5("关于TW页面标题").".html");
 		$this->display("./index");
     }
 }
